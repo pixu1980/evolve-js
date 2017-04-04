59fcf0a47ae0da1282ce9f233d95241f362c7fd4
bwbuser <bwbuser2@iMacBlack.local>
2017-04-04 12:06:49 +0200

    - first commit   - v0.0.1

--- webpack.config.js
+++ webpack.config.js
@@ -42,11 +42,15 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules|bower_components/,
         loader: 'babel-loader',
-        'query': {
-          'presets': [['env', {
-            'modules': false,
-            'targets': {'node': 4}
-          }]]
+        query: {
+          presets: [
+            ['env', {
+              modules: false,
+              targets: {
+                node: 4
+              }
+            }]
+          ]
         }
       },
     ],
