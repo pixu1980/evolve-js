640fbd6d148ab192af15963fed9e707e1951f891
bwbuser <bwbuser2@iMacBlack.local>
2017-05-03 10:44:46 +0200

    - v0.1.9, modified element renderig flux

--- src/Elements/Element.js
+++ src/Elements/Element.js
@@ -1,4 +1,4 @@
-import {Container, Shadow, Shape} from '../CreateJS/EaselJS';
+import { Container, Shadow, Shape, DisplayObject } from '../CreateJS/EaselJS';
 import ElementHelpers from './ElementHelpers';
 
 /**
@@ -19,13 +19,13 @@ export default class Element extends Container {
 
     this.initData();
 
-    if(this.settings.parent) {
+    if (this.settings.parent) {
       this.settings.parent.addChild(this);
     }
 
-    this.drawElements();
+    this.draw();
 
-    if(!!this.settings.align) {
+    if (!!this.settings.align) {
       this.align(null, this.settings.align);
     }
 
@@ -69,6 +69,7 @@ export default class Element extends Container {
           width: 0,
           height: 0,
         },
+        cache: false,
         mask: false,
         fill: 'rgba(255,255,255,0.001)',
         stroke: {
@@ -109,84 +110,60 @@ export default class Element extends Container {
     this.data = {};
   }
 
-  /**
-   * draws all graphic elements of the Element instance
-   * @memberOf Element
-   * @method drawElements
-   * @instance
-   */
-  drawElements() {
+  preDrawElements() {
     this.setScale(this.settings.scale);
+    this.setComputedBounds(this.settings.size);
+  }
 
-    if(!this.bounds && !!this.settings.size) {
-      if(String.isPercentage(this.settings.size)) {
-        this.settings.size = {
-          width: '100%',
-          height: '100%',
-        };
-      } else if(Number.isNumber(this.settings.size)) {
-        this.settings.size = {
-          width: this.settings.size,
-          height: this.settings.size,
-        };
-      }
-
-      if(String.isPercentage(this.settings.size.width) && !!this.parent) {
-        this.settings.size.width = this.parent.getComputedBounds().width * this.settings.size.width.parsePercentage();
-      }
-
-      if(String.isPercentage(this.settings.size.height) && !!this.parent) {
-        this.settings.size.height = this.parent.getComputedBounds().height * this.settings.size.height.parsePercentage();
-      }
-
-      this.bounds = [0, 0, this.settings.size.width * this.settings.scale.x, this.settings.size.height * this.settings.scale.y];
-    }
-
+  drawBackgroundElement() {
     this.background = ElementHelpers.createRect(this.settings.pick(['fill', 'stroke']), ...this.bounds);
     this.addChild(this.background);
-    this.setChildIndex(this.background, 0);
+  }
 
-    if(!!this.settings.regPoint) {
+  drawElements() {
+    //TODO:
+  }
+
+  drawBehaviorElements() {
+    this.setMask(this.settings.mask);
+    this.setShadow(this.settings.shadow);
+    this.setCache(this.settings.cache);
+  }
+
+  drawOverlayElements() {
+    if (!!this.settings.regPoint) {
       this.regPoint = ElementHelpers.createCircle(this.settings.regPoint, ...this.bounds);
       this.addChild(this.regPoint);
-      this.setChildIndex(this.regPoint, this.children.length - 1);
 
       ElementHelpers.align(this.regPoint, null, 'center middle', false);
     }
+  }
 
+  postDrawElement() {
     this.setBounds(...this.bounds);
-
     this.setReg();
     this.setPosition(this.settings.position);
-    this.setShadow(this.settings.shadow);
-
-    if(!!this.settings.mask) {
-      this.maskShape = this.settings.mask;
-
-      if(!(this.settings.mask instanceof Shape) && !(this.settings.mask instanceof Container)) {
-        this.maskBounds = [0, 0, this.settings.size.width * this.settings.scale.x, this.settings.size.height * this.settings.scale.y];
-
-        if(Object.isObject(this.settings.mask)) {
-          this.maskBounds = [0, 0, this.settings.size.width * this.settings.scale.x * this.settings.mask.scale, this.settings.size.height * this.settings.scale.y * this.settings.mask.scale];
-          this.maskShape = ElementHelpers.createRect(this.settings.mask.pick(['fill', 'stroke']), ...this.maskBounds);
-        } else if(Number.isNumber(this.settings.mask)) {
-          this.maskBounds = [0, 0, this.settings.size.width * this.settings.scale.x * this.settings.mask, this.settings.size.height * this.settings.scale.y * this.settings.mask];
-          this.maskShape = ElementHelpers.createRect(this.settings.pick(['fill', 'stroke']), ...this.maskBounds);
-        }
-
-        this.mask = this.maskShape.inherit({
-          x: this.bounds[2] * 0.5 - this.maskBounds[2] * 0.5,
-          y: this.bounds[3] * 0.5 - this.maskBounds[3] * 0.5,
-        });
-      }
-
-      if(!!this.settings.debug) {
-        this.addChild(this.maskShape);
-      } else {
-        this.mask = this.maskShape;
-      }
-    }
   }
+
+  /**
+   * draws all graphic elements of the Element instance
+   * @memberOf Element
+   * @method draw
+   * @instance
+   */
+  draw() {
+    super.draw();
+
+    this.preDrawElements();
+
+    this.drawBackgroundElements();
+    this.drawElements();
+    this.drawBehaviorElements();
+    this.drawOverlayElements();
+
+    this.postDrawElements();
+  }
+
 
   /**
    * sets to center/middle the regPoint of the Element instance
@@ -208,26 +185,37 @@ export default class Element extends Container {
    * @memberOf Element
    * @method setPosition
    * @instance
-   * @param {Object} position can contain x and y or only one or them
-   * @param {Number} [position.x=0] the x position
-   * @param {Number} [position.y=0] the y position
+   * @param {Object} [positionOptions=null] can contain x and y or only one or them
+   * @param {Number} [positionOptions.x=0] the x position
+   * @param {Number} [positionOptions.y=0] the y position
    * @param {Boolean} [override=false] specify to override actual Element position
+   * @param {Boolean} [force=true] ...
    * @return {Element} to make chainable the method
    */
-  setPosition(position = {x: 0, y: 0}, override = false) {
-    if(!!position.x) {
-      if(!!override) {
-        this.x = position.x;
+  setPosition(positionOptions = null, override = false, force = true) {
+    if (!!positionOptions) {
+      if (force) {
+        this.settings.position = positionOptions;
       } else {
-        this.x += position.x;
+        this.settings.position.inherit(positionOptions);
       }
     }
 
-    if(!!position.y) {
-      if(!!override) {
-        this.y = position.y;
-      } else {
-        this.y += position.y;
+    if (!!this.settings.position) {
+      if (!!this.settings.position.x) {
+        if (!!override) {
+          this.x = this.settings.position.x;
+        } else {
+          this.x += this.settings.position.x;
+        }
+      }
+
+      if (!!this.settings.position.y) {
+        if (!!override) {
+          this.y = this.settings.position.y;
+        } else {
+          this.y += this.settings.position.y;
+        }
       }
     }
 
@@ -242,29 +230,127 @@ export default class Element extends Container {
    * @param {Object|Number} scaleOptions can be an object with x and y couple or only a number to be used for both
    * @return {Element} to make chainable the method
    */
-  setScale(scaleOptions = {x: 1, y: 1}) {
-    let scaleSettings = scaleOptions;
-
-    if(Number.isNumber(scaleSettings)) {
-      scaleSettings = {
-        scaleX: scaleSettings,
-        scaleY: scaleSettings,
-      };
-    } else if(Object.isObject(scaleSettings)) {
-      scaleSettings = {
-        scaleX: scaleSettings.x,
-        scaleY: scaleSettings.y,
-      };
+  setScale(scaleOptions = null, force = false) {
+    if (!!scaleOptions) {
+      if (force) {
+        this.settings.scale = scaleOptions;
+      } else {
+        this.settings.scale.inherit(scaleOptions);
+      }
     }
 
-    this.settings.inherit({
-      scale: {
-        x: scaleSettings.scaleX,
-        y: scaleSettings.scaleY,
-      },
-    });
+    if (!!this.settings.scale) {
+      const scale = {
+        scaleX: 1,
+        scaleY: 1,
+      };
 
-    this.inherit(scaleSettings);
+      if (Number.isNumber(this.settings.scale)) {
+        scale.inherit({
+          scaleX: this.settings.scale,
+          scaleY: this.settings.scale,
+        });
+      } else if (Object.isObject(this.settings.scale)) {
+        scale.inherit({
+          scaleX: this.settings.scale.x,
+          scaleY: this.settings.scale.y,
+        });
+      }
+
+      this.inherit(scale);
+    }
+
+    return this;
+  }
+
+  /**
+   * sets or updates the bounds of the Element instance
+   * @memberOf Element
+   * @method setComputedBounds
+   * @instance
+   * @param {Object|Number} boundsOptions ...
+   * @return {Element} to make chainable the method
+   */
+  setComputedBounds(boundsOptions = null, force = false) {
+    if (!!boundsOptions) {
+      if (force) {
+        this.settings.size = boundsOptions;
+      } else {
+        this.settings.size.inherit(boundsOptions);
+      }
+    }
+
+    if (!this.bounds && !!this.settings.size) {
+      if (String.isPercentage(this.settings.size)) {
+        this.settings.size = {
+          width: '100%',
+          height: '100%',
+        };
+      } else if (Number.isNumber(this.settings.size)) {
+        this.settings.size = {
+          width: this.settings.size,
+          height: this.settings.size,
+        };
+      }
+
+      if (String.isPercentage(this.settings.size.width) && !!this.parent) {
+        this.settings.size.width = this.parent.getComputedBounds().width * this.settings.size.width.parsePercentage();
+      }
+
+      if (String.isPercentage(this.settings.size.height) && !!this.parent) {
+        this.settings.size.height = this.parent.getComputedBounds().height * this.settings.size.height.parsePercentage();
+      }
+
+      this.bounds = [0, 0, this.settings.size.width * this.settings.scale.x, this.settings.size.height * this.settings.scale.y];
+    }
+
+    return this;
+  }
+
+  /**
+   * sets or updates the Mask of the Element instance
+   * @memberOf Element
+   * @method setMask
+   * @instance
+   * @param {Object|Shadow} maskOptions can be an object with {color, x, y, blur}<br>or directly an instance of Shadow (EaselJS)
+   * @param {Boolean} force ....
+   * @return {Element} to make chainable the method
+   */
+  setMask(maskOptions = null, force = false) {
+    if (!!maskOptions) {
+      if (force) {
+        this.settings.mask = maskOptions;
+      } else {
+        this.settings.mask.inherit(maskOptions);
+      }
+    }
+
+    if (!!this.settings.mask) {
+      this.maskShape = this.settings.mask;
+
+      if (!(this.settings.mask instanceof Shape) && !(this.settings.mask instanceof DisplayObject)) {
+        this.maskBounds = [0, 0, this.settings.size.width * this.settings.scale.x, this.settings.size.height * this.settings.scale.y];
+
+        if (Object.isObject(this.settings.mask)) {
+          this.maskBounds = [0, 0, this.settings.size.width * this.settings.scale.x * this.settings.mask.scale, this.settings.size.height * this.settings.scale.y * this.settings.mask.scale];
+          this.maskShape = ElementHelpers.createRect(this.settings.mask.pick(['fill', 'stroke']), ...this.maskBounds);
+        } else if (Number.isNumber(this.settings.mask)) {
+          this.maskBounds = [0, 0, this.settings.size.width * this.settings.scale.x * this.settings.mask, this.settings.size.height * this.settings.scale.y * this.settings.mask];
+          this.maskShape = ElementHelpers.createRect(this.settings.pick(['fill', 'stroke']), ...this.maskBounds);
+        }
+
+        this.maskShape.inherit({
+          x: this.bounds[2] * 0.5 - this.maskBounds[2] * 0.5,
+          y: this.bounds[3] * 0.5 - this.maskBounds[3] * 0.5,
+        });
+      }
+
+      if (!!this.settings.debug) {
+        this.addChild(this.maskShape);
+      }
+
+      this.mask = this.maskShape;
+    }
 
     return this;
   }
@@ -275,16 +361,21 @@ export default class Element extends Container {
    * @method setShadow
    * @instance
    * @param {Object|Shadow} shadowOptions can be an object with {color, x, y, blur}<br>or directly an instance of Shadow (EaselJS)
+   * @param {Boolean} force ....
    * @return {Element} to make chainable the method
    */
-  setShadow(shadowOptions = null) {
-    if(!!shadowOptions) {
-      this.settings.shadow = shadowOptions;
+  setShadow(shadowOptions = null, force = false) {
+    if (!!shadowOptions) {
+      if (force) {
+        this.settings.shadow = shadowOptions;
+      } else {
+        this.settings.shadow.inherit(shadowOptions);
+      }
 
-      if(!!this.settings.shadow) {
+      if (!!this.settings.shadow) {
         let shadow = null;
 
-        if(Object.isObject(this.settings.shadow)) {
+        if (Object.isObject(this.settings.shadow)) {
           const shadowSettings = [
             this.settings.shadow.color,
             this.settings.shadow.x,
@@ -293,7 +384,7 @@ export default class Element extends Container {
           ];
 
           shadow = new Shadow(...shadowSettings);
-        } else if(this.settings.shadow instanceof Shadow) {
+        } else if (this.settings.shadow instanceof Shadow) {
           shadow = this.settings.shadow;
         }
 
@@ -312,6 +403,31 @@ export default class Element extends Container {
   }
 
   /**
+   * sets or updates the Cache of the Element instance
+   * @memberOf Element
+   * @method setCache
+   * @instance
+   * @param {Object|Shadow} cacheOptions ..
+   * @param {Boolean} force ....
+   * @return {Element} to make chainable the method
+   */
+  setCache(cacheOptions = null, force = false) {
+    if (!!cacheOptions) {
+      if (force) {
+        this.settings.cache = cacheOptions;
+      } else {
+        this.settings.cache.inherit(cacheOptions);
+      }
+
+      if (!!this.settings.cache) {
+        this.cache(...[this.bounds.x - 5, this.bounds.y - 5, this.getComputedBounds().width, this.getComputedBounds().height]);
+      }
+    }
+
+    return this;
+  }
+
+  /**
    * Aligns the Element instance, based on it's parent bounds or specified parentElement bounds and mode parameter
    * @memberOf Element
    * @method align
@@ -321,7 +437,7 @@ export default class Element extends Container {
    * @return {Element} to make chainable the method
    */
   align(parentElement = null, mode = 'left top') {
-    if(!parentElement) {
+    if (!parentElement) {
       parentElement = this.parent;
     }
 
@@ -336,32 +452,32 @@ export default class Element extends Container {
       y: 0,
     };
 
-    if(horizontalModes.contains(modes)) {
-      if(modes.contains('left')) {
+    if (horizontalModes.contains(modes)) {
+      if (modes.contains('left')) {
         position.inherit({
           x: bounds.width * 0.5,
         });
-      } else if(modes.contains('center')) {
+      } else if (modes.contains('center')) {
         position.inherit({
           x: parentBounds.width * 0.5,
         });
-      } else if(modes.contains('right')) {
+      } else if (modes.contains('right')) {
         position.inherit({
           x: parentBounds.width - bounds.width * 0.5,
         });
       }
     }
 
-    if(verticalModes.contains(modes)) {
-      if(modes.contains('top')) {
+    if (verticalModes.contains(modes)) {
+      if (modes.contains('top')) {
         position.inherit({
           y: bounds.height * 0.5,
         });
-      } else if(modes.contains('middle')) {
+      } else if (modes.contains('middle')) {
         position.inherit({
           y: parentBounds.height * 0.5,
         });
-      } else if(modes.contains('bottom')) {
+      } else if (modes.contains('bottom')) {
         position.inherit({
           y: parentBounds.height - bounds.height * 0.5,
         });
@@ -420,9 +536,9 @@ export default class Element extends Container {
    * @instance
    */
   bindEvents() {
-    if(!!this.settings.events) {
+    if (!!this.settings.events) {
       this.settings.events.each((value, key) => {
-        if(Function.isFunction(value)) {
+        if (Function.isFunction(value)) {
           this.on(key, value);
         }
       });
